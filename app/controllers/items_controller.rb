ffc48eb2b2f49353cd1b047ff48101dc461d1c5c
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-01-08 19:30:47 +0900

    ユーザー管理機能の実装（新規登録、ログイン、ログアウト）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,13 @@ class ItemsController < ApplicationController
   def index
   end
 
+  # def new
+  # end
+
+  # def create
+  # end
+  #  def show
+  #    @user = User.find(params[:id])
+  #  end
+
 end

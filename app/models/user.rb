ea9ad28e8e694c322eb1ba7be882650aa5b63c17
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-01-15 14:56:54 +0900

    ロボコップの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  #has_many  :purchases
+  # has_many  :purchases
 
-  validates :nickname, presence: true,  length: { maximum: 6 }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には6文字以上の半角英字と半角数字の両方を含めて設定してください'          
-  
+  validates :nickname, presence: true, length: { maximum: 6 }
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には6文字以上の半角英字と半角数字の両方を含めて設定してください'
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角（漢字、ひらがな、カタカナ）文字を使用してください' } do
     validates :first_name
     validates :last_name
@@ -21,4 +21,3 @@ class User < ApplicationRecord
   end
   validates :birth_date, presence: true
 end
-
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.free_email}
-    password              {"ab1" + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            {"山田"}
-    last_name             {"太郎"}
-    first_name_kana       {"ヤマダ"}
-    last_name_kana        {"タロウ"}
-    birth_date            {"1999-12-11"}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { 'ab1' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { '山田' }
+    last_name             { '太郎' }
+    first_name_kana       { 'ヤマダ' }
+    last_name_kana        { 'タロウ' }
+    birth_date            { '1999-12-11' }
   end
 end

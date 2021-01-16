bdcce0de1a80049526b62c8ec5c0554bea5e7d5c
kodairafumikazu <z27d6a27afh862000@yahoo.co.jp>
2021-01-16 20:44:20 +0900

    ロボコップの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update]
-  #before_action :authenticate_user!, except: [:index, :show]
+  
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -16,21 +16,17 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if @item.user.id != current_user.id  
-      redirect_to root_path
-    end
-    
-
+    redirect_to root_path if @item.user.id != current_user.id
   end
 
   def update
     item = Item.find(params[:id])
     item.update(item_params)
-     if current_user.update(user_params)
-       redirect_to root_path
-     else
-       render :edit
-     end
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   # def create
@@ -50,8 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
